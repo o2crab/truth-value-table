169d7a2554a0bfc6bd020d3d969e494f25467328
o2crab <bio12kbb@gmail.com>
2023-02-27 23:42:24 +0900

    add Alphabet 'true()' and 'false()'

--- src/alphabet.rs
+++ src/alphabet.rs
@@ -1,5 +1,7 @@
 pub enum Alphabet {
     Letter(char),
+    True,
+    False,
     Negation,
     Conjunction,
     Disjunction,
@@ -11,6 +13,8 @@ impl std::fmt::Display for Alphabet {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Alphabet::Letter(c)     => write!(f, "{}", c),
+            Alphabet::True          => write!(f, "⊤"), // \u{22A4}
+            Alphabet::False         => write!(f, "⊥"), // \u{22A5}
             Alphabet::Negation      => write!(f, "¬"), // \u{00AC}
             Alphabet::Conjunction   => write!(f, "∧"), // \u{2227}
             Alphabet::Disjunction   => write!(f, "∨"), // \u{2228}
--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,21 @@ fn main() {
     let formula = vec![
         Alphabet::Letter('P'),
         Alphabet::Conjunction,
+        Alphabet::Negation,
         Alphabet::Letter('Q'),
         Alphabet::Implicature,
         Alphabet::Letter('R'),
+        Alphabet::Disjunction,
+        Alphabet::Letter('S'),
     ];
 
     for x in formula {
         print!("{} ", x);
     }
     println!();
+
+    println!("{}, {}",
+        Alphabet::True,
+        Alphabet::False,
+    );
 }
